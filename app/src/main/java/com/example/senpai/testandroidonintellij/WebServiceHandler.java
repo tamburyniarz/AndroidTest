2bfc79eed021182b0c573d470cda842f92743d25
tamburyniarz <tamburyniarz@gmail.com>
2016-07-30 16:08:55 +0200

    Init

---
+++ app/src/main/java/com/example/senpai/testandroidonintellij/WebServiceHandler.java
@@ -0,0 +1,15 @@
+package com.example.senpai.testandroidonintellij;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Senpai on 30/07/2016.
+ */
+public class WebServiceHandler extends AsyncTask<String, Void, String> {
+    
+
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
